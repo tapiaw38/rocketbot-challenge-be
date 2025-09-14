744e44a32ae82d2d1d86273d40b1fe7dd9800f5c
Walter Tapia <tapiaw38@gmail.com>
2025-09-14 18:22:59 -0300

    Fix conflict insort

--- src/routes/routes_manager.py
+++ src/routes/routes_manager.py
@@ -3,8 +3,7 @@ from fastapi import FastAPI
 from src.adapters.datasources.datasources import Datasources
 from src.adapters.services.task.task_service import TaskService
 from src.adapters.web.controllers.task.task_controller import get_task_service
-from src.adapters.web.controllers.task.task_controller import \
-    router as task_router
+from src.adapters.web.controllers.task.task_controller import router as task_router
 from src.core.platform.appcontext.appcontext import new_factory
 from src.core.use_cases.use_cases import create_usecases
 
